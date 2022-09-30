269b74223c0d0fd21fceec218b2b3d890b3d06fd
Nicolas Sá de Souza <nicolassouza@atendare.com>
2022-09-30 20:40:48 -0300

    Added client.go

---
+++ client/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	output    = make(chan string)
+	input     = make(chan string)
+	errorChan = make(chan error)
+)
+
+func readStdin() {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		m, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+		input <- m
+	}
+}
+
+func readConn(conn net.Conn) {
+	for {
+		reader := bufio.NewReader(conn)
+		m, err := reader.ReadString('\n')
+		if err != nil {
+			errorChan <- err
+			return
+		}
+		output <- m
+	}
+}
+
+func connect() net.Conn {
+	var (
+		conn net.Conn
+		err  error
+	)
+	for {
+		fmt.Println("Connecting to server...")
+		conn, err = net.Dial("tcp", ":6666")
+		if err == nil {
+			break
+		}
+		fmt.Println(err)
+		time.Sleep(time.Second * 1)
+	}
+	fmt.Println("Connection accepted")
+	return conn
+}
+
+func main() {
+
+	go readStdin()
+
+RECONNECT:
+	for {
+		conn := connect()
+
+		go readConn(conn)
+
+		for {
+			select {
+			case m := <-output:
+				fmt.Printf("--> Servidor: %q\n", strings.Trim(m, "\r\n"))
+
+			case m := <-input:
+				// fmt.Printf("Sending: %q\n", m)
+				_, err := conn.Write([]byte(m + "\n"))
+				if err != nil {
+					fmt.Println(err)
+					conn.Close()
+					continue RECONNECT
+				}
+			case err := <-errorChan:
+				fmt.Println("Error:", err)
+				conn.Close()
+				continue RECONNECT
+			}
+		}
+	}
+}
